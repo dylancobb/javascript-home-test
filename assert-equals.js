9e9041353b5485f59da1255e2144cc2d72231f62
Hex <hexmusictheory@protonmail.com>
2023-07-22 15:15:04 +0100

    preliminary function + tests

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,45 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+    // throw an error if types differ
+    if (typeof expect !== typeof actual) {
+        throw new Error(`expected ${typeof expect} but found ${typeof actual}`)
+    }
+
+    // handle primitive types
+    if (typeof expect !== 'object') {
+        if (expect !== actual) {
+            throw new Error(`expected ${expect} but found ${actual}`)
+        }
+        return true;
+    }
+
+    // handle arrays
+    if (Array.isArray(expect)) {
+        if (expect.length !== actual.length) {
+            throw new Error(`expected ${JSON.stringify(expect)} but found ${JSON.stringify(actual)}`)
+        }
+        // compare elements
+        for (let i = 0; i < expect.length; i++) {
+            if (!assertEquals(expect[i], actual[i])) {
+                throw new Error(`expected ${JSON.stringify(expect)} but found ${JSON.stringify(actual)}`)
+            }
+        }
+        return true;
+    }
+
+    // handle objects
+    const expectKeys = Object.keys(expect);
+    const actualKeys = Object.keys(actual);
+
+    if (expectKeys.length !== actualKeys.length) {
+        throw new Error(`expected ${JSON.stringify(expect)} but found ${JSON.stringify(actual)}`)
+    }
+
+    for (const key of expectKeys) {
+        if (!actual.hasOwnProperty(key) || !assertEquals(expect[key], actual[key]))
+            throw new Error(`expected ${JSON.stringify(expect)} but found ${JSON.stringify(actual)}`)
+    }
+
+    return true;
+}
 
 module.exports = assertEquals
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,89 @@
 const assertEquals = require('./assert-equals')
 
 describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
+  describe('when expected and actual types are different', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals(123, '123')).toThrow()
     })
   })
 
-  describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
+  describe('when expected and actual are the same primitive type', () => {
+    describe('when they are identical', () => {
+      it('returns without throwing an error', () => {
+        expect(() => assertEquals(123, 123)).not.toThrow()
+        expect(() => assertEquals('abc', 'abc')).not.toThrow()
+        expect(() => assertEquals(true, true)).not.toThrow()
+        expect(() => assertEquals(undefined, undefined)).not.toThrow()
+      })
+    })
+  
+    describe('when they differ', () => {
+      it('throws an error', () => {
+        expect(() => assertEquals(123, 1234)).toThrow()
+        expect(() => assertEquals('abc', 'abcd')).toThrow()
+        expect(() => assertEquals(true, false)).toThrow()
+        expect(() => assertEquals(undefined, 'abc')).toThrow()
+      })
+    })
+  })
+
+  describe('when expected and actual are arrays', () => {
+    describe('when their lengths differ', () => {
+      it('throws an error', () => {
+        expect(() => assertEquals([1, 2], [1, 2, 3])).toThrow()
+      })
+    })
+    describe('when their (primitive) elements are identical', () => {
+      it('does not throw an error', () => {
+        expect(() => assertEquals([1, 2], [1, 2])).not.toThrow()
+        expect(() => assertEquals(['a', 'b'], ['a', 'b'])).not.toThrow()
+        expect(() => assertEquals([true, false], [true, false])).not.toThrow()
+      })
+    })
+    describe('when their (primitive) elements are identical', () => {
+      it('does not throw an error', () => {
+        expect(() => assertEquals([1, 'a'], [1, 2])).toThrow()
+        expect(() => assertEquals(['a', 2], ['a', 'b'])).toThrow()
+        expect(() => assertEquals([true, true], [true, false])).toThrow()
+      })
+    })
+    describe('when their elements are identical (nested arrays)', () => {
+      it('does not throw an error', () => {
+        expect(() => assertEquals([1, [1, 2]], [1, [1, 2]])).not.toThrow()
+      })
+    })
+    describe('when their elements differ (nested arrays)', () => {
+      it('throws an error', () => {
+        expect(() => assertEquals([1, ['a', 2]], [1, [1, 2]])).toThrow()
+      })
+    })
+    describe('when their elements are identical (nested objects)', () => {
+      it('does not throw an error', () => {
+        expect(() => assertEquals([1, {two: 2}], [1, {two: 2}])).not.toThrow()
+      })
+    })
+    describe('when their elements differ (nested objects)', () => {
+      it('throws an error', () => {
+        expect(() => assertEquals([1, {three: 3}], [1, {two: 2}])).toThrow()
+      })
+    })
+  })
+
+  describe('when expected and actual are objects', () => {
+    describe('when expected and actual have different number of properties', () => {
+      it('throws an error', () => {
+        expect(() => assertEquals({one: 1}, {one: 1, two: 2})).toThrow()
+      })
+    })
+    describe('when expected and actual have different property names', () => {
+      it('throws an error', () => {
+        expect(() => assertEquals({one: 1, two: 2}, {one: 1, three: 3})).toThrow()
+      })
+    })
+    describe('when expected and actual have different property values', () => {
+      it('throws an error', () => {
+        expect(() => assertEquals({one: 1, two: 2}, {one: 1, two: 3})).toThrow()
+      })
+    })
   })
 })
